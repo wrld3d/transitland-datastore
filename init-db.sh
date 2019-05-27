ec793c7da0089913378768bcaec874949f1a2719
Michael O'Donnell <michael.odonnell@wrld3d.com>
2019-05-27 09:28:14 +0100

    Optional -y on init-db to proceed automatically Buddy Jonty Dawson

--- init-db.sh
+++ init-db.sh
@@ -10,11 +10,33 @@ function trap_failure {
 trap 'trap_failure $LINENO' ERR
 set -eu
 
+do_continue_automatically=False
+
+while getopts "y" o; do
+    case "${o}" in
+        y)
+            do_continue_automatically=True
+            ;;
+    esac
+done
+
+if [ -z "$TRANSITLAND_TOKEN" ]; then
+    echo "No TRANSITLAND_TOKEN environment variable set"
+    exit 1
+fi
+
+if [ -z "$TRANSITLAND_ENV" ]; then
+    echo "No TRANSITLAND_ENV environment variable set"
+    exit 1
+fi
+
+if [ $do_continue_automatically == False ]; then
 echo "This is a destructive operation.  The database will have been setup when the AMI was built, this does not need to be run again unless you intend to clear all operators, feeds, routes and stops"
 echo "Do you want to continue? yes/no"
-
 read do_continue
-if [[ "$do_continue" == "y" ]]  || [[ "$do_continue" == "yes" ]]; then
+fi
+
+if [[ $do_continue_automatically == True ]] || [[ "$do_continue" == "y" ]]  || [[ "$do_continue" == "yes" ]]; then
   docker-compose run app bash ./wait-for-it.sh -h db -p 5432 -- bundle exec rake db:drop db:create db:setup;
   exit 0;
 else
